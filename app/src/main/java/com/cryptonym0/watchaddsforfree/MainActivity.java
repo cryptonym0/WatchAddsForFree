ea840fa2b7268a5714a9d0a4684e9475595435fc
cryptonym0 <elyciabuckley@msn.com>
2017-03-07 10:03:30 -0800

    added dependancies

--- app/src/main/java/com/cryptonym0/watchaddsforfree/MainActivity.java
+++ app/src/main/java/com/cryptonym0/watchaddsforfree/MainActivity.java
@@ -2,12 +2,19 @@ package com.cryptonym0.watchaddsforfree;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
 import com.google.android.gms.ads.MobileAds;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.firebase.messaging.FirebaseMessaging;
 
 
 public class MainActivity extends AppCompatActivity {
+    //variables
+    private static final int PLAY_SERVICES_RESOLUTION_REQUEST = 9000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +27,35 @@ public class MainActivity extends AppCompatActivity {
         AdRequest adRequest = new AdRequest.Builder().build();
         mAdView.loadAd(adRequest);
 
+        //Add topic
+        FirebaseMessaging.getInstance().subscribeToTopic("news");
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        checkPlayServices();
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    private boolean checkPlayServices() {
+        GoogleApiAvailability apiAvailability = GoogleApiAvailability.getInstance();
+        int resultCode = apiAvailability.isGooglePlayServicesAvailable(this);
+        if (resultCode != ConnectionResult.SUCCESS) {
+            if (apiAvailability.isUserResolvableError(resultCode)) {
+                apiAvailability.getErrorDialog(this, resultCode, PLAY_SERVICES_RESOLUTION_REQUEST).show();
+            } else {
+                Log.i("GOOGLE PLAY", "This device is not supported.");
+                finish();
+            }
+            return false;
+        }
+        return true;
+    }
+
 
 
 }
---
+++ app/src/main/java/com/cryptonym0/watchaddsforfree/MyFirebaseIDService.java
@@ -0,0 +1,23 @@
+package com.cryptonym0.watchaddsforfree;
+
+import android.app.DownloadManager;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static com.google.android.gms.internal.zzs.TAG;
+
+/**
+ * Created by t00055219 on 3/7/2017.
+ */
+
+public class MyFirebaseIDService extends FirebaseInstanceIdService {
+
+
+
+}
